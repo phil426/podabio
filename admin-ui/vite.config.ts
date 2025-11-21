7a69c1274e16a066b58ebd0316e3abbe035e5d58
phil426 <phil@redwoodempiremedia.com>
2025-11-21 07:45:14 -0800

    Add base path to Vite config for production builds

--- admin-ui/vite.config.ts
+++ admin-ui/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/admin-ui/dist/',
   server: {
     port: 5174,
     host: '0.0.0.0'
